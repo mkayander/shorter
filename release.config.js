6f8e10f887a2a7ea3f2e8a08365aea9fdd82dbb1
mkayander <maxim.kayander1@gmail.com>
2022-10-16 22:22:45 +0200

    feat: Added semantic-release versioning

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    branches: ['main', 'next'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'yarn.lock'],
+            },
+        ],
+        '@semantic-release/github',
+    ],
+};
