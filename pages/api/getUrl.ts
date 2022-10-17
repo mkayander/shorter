75c9b0e4bae66aebe86f8245c15affe976e8d16b
mkayander <maxim.kayander1@gmail.com>
2022-10-17 23:05:31 +0200

    feat: Added basic getUrl api endpoint

---
+++ pages/api/getUrl.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@src/server/prisma";
+
+const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  console.log({ slug });
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 400;
+
+    res.send({ message: "Slug is not specified! ⚠" });
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+
+    res.send({ message: "Slug not found!" });
+    return;
+  }
+
+  return res.redirect(data.url);
+};
+
+export default getUrl;
